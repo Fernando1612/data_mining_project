ba168955335c92d92286d1376fb8e9cad93a72c5
Cesar Yair <noctan_lovo@hotmail.com>
2023-05-23 19:58:54 -0600

    front fixed
    Falta correlacionar bien los datos

--- lib/exploracion.dart
+++ lib/exploracion.dart
@@ -2,34 +2,46 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-
 class ExploracionDatos extends StatefulWidget {
   @override
   _ExploracionDatosState createState() => _ExploracionDatosState();
 }
 
 class _ExploracionDatosState extends State<ExploracionDatos> {
-  String apiUrl= 'http://127.0.0.1:5000/data-preview';
   List<String> columnNames = [];
   List<Map<String, dynamic>> data = [];
+
+  Future<void> fetchDataPreview() async {
+    try {
+      final fetchedData = await fetchData();
+      setState(() {
+        columnNames = fetchedData['columnNames'];
+        data = fetchedData['data'];
+      });
+    } catch (e) {
+      // Handle the error
+    }
+  }
+
+  Future<Map<String, dynamic>> fetchData() async {
+    final response = await http.get(Uri.parse('http://127.0.0.1:5000/data-preview'));
+    if (response.statusCode == 200) {
+      final jsonData = json.decode(response.body);
+      final columnNames = List<String>.from(jsonData['column_names']);
+      final data = List<Map<String, dynamic>>.from(jsonData['data']);
+      return {
+        'columnNames': columnNames,
+        'data': data,
+      };
+    } else {
+      throw Exception('Failed to fetch data');
+    }
+  }
 
   @override
   void initState() {
     super.initState();
-    fetchData();
-  }
-
-  void fetchData() async {
-    final response = await http.get(Uri.parse(apiUrl));
-    if (response.statusCode == 200) {
-      final jsonData = json.decode(response.body);
-      setState(() {
-        columnNames = List<String>.from(jsonData['column_names']);
-        data = List<Map<String, dynamic>>.from(jsonData['data']);
-      });
-    } else {
-      throw Exception('Failed to fetch data');
-    }
+    fetchDataPreview();
   }
 
   @override
@@ -41,7 +53,9 @@ class _ExploracionDatosState extends State<ExploracionDatos> {
       body: Container(
         padding: EdgeInsets.all(16.0),
         child: DataTable(
-          columns: columnNames.map((name) => DataColumn(label: Text(name))).toList(),
+          columns: columnNames
+              .map((name) => DataColumn(label: Text(name)))
+              .toList(),
           rows: data.map((rowData) {
             return DataRow(
               cells: rowData.entries.map((entry) {
